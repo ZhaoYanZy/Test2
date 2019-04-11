e392e380fd26499879c5214e939cacfceee8fdcb
zy <15013661810@163.com>
2019-04-11 17:07:52 +0800

    清楚无用包

--- app/src/main/java/com/example/test2/MainActivity.java
+++ app/src/main/java/com/example/test2/MainActivity.java
@@ -1,10 +1,9 @@
 package com.example.test2;
 
-import android.app.Activity;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.widget.TextView;
 
 import java.lang.ref.WeakReference;
