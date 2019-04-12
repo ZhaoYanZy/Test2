1e1c264012dedf65b8191ea74792a7373f01bffe
zy <15013661810@163.com>
2019-04-12 09:10:32 +0800

    新增测试Activity

---
+++ app/src/main/java/com/example/test2/TestActivity.java
@@ -0,0 +1,18 @@
+package com.example.test2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 测试Activity
+ */
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+
+    }
+}
