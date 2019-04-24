f615f24eb2428962c7908c2f05fc31374edea133
zy <15013661810@163.com>
2019-04-24 18:21:32 +0800

    添加网络工具类

---
+++ app/src/main/java/com/example/test2/OkHttpUtils.java
@@ -0,0 +1,19 @@
+package com.example.test2;
+
+import okhttp3.OkHttpClient;
+
+/**
+ * @author Created by zy
+ * @date 2019/4/12 10:07
+ * @pag com.example.test2
+ * @des TODO
+ * @org hx
+ * @email zhaoyan_163email@163.com
+ */
+public class OkHttpUtils {
+
+    private void getRequest() {
+        OkHttpClient httpClient = new OkHttpClient();
+    }
+
+}
